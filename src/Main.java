a5ab578f38aabe39f86747c75b3b87bd0c5ee028
Alexander Klimenkov <alik.klimenkov@gmail.com>
2016-12-10 17:49:56 +0200

    Jsoup library added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 /**
  * Created by klimenkov on 10.12.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hey!");
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("http://en.wikipedia.org/").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements newsHeadlines = doc.select("#mp-itn b a");
     }
 }
